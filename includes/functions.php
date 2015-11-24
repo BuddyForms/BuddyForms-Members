5b7f80dee50622bd4ebe7fdd63b9cb3372807037
Kishore <babunsahoo@gmail.com>
2015-11-24 16:45:29 +0530

    fix for editlink

--- includes/functions.php
+++ includes/functions.php
@@ -200,4 +200,20 @@ function buddyforms_members_button_add_new($button,$args){
 	return $button;
 }
 
+// for more info: http://stackoverflow.com/questions/6768793/get-the-full-url-in-php
+function url_origin( $s, $use_forwarded_host = false ) {
+    $ssl      = ( ! empty( $s['HTTPS'] ) && $s['HTTPS'] == 'on' );
+    $sp       = strtolower( $s['SERVER_PROTOCOL'] );
+    $protocol = substr( $sp, 0, strpos( $sp, '/' ) ) . ( ( $ssl ) ? 's' : '' );
+    $port     = $s['SERVER_PORT'];
+    $port     = ( ( ! $ssl && $port=='80' ) || ( $ssl && $port=='443' ) ) ? '' : ':'.$port;
+    $host     = ( $use_forwarded_host && isset( $s['HTTP_X_FORWARDED_HOST'] ) ) ? $s['HTTP_X_FORWARDED_HOST'] : ( isset( $s['HTTP_HOST'] ) ? $s['HTTP_HOST'] : null );
+    $host     = isset( $host ) ? $host : $s['SERVER_NAME'] . $port;
+    return $protocol . '://' . $host;
+}
+
+function full_url( $s, $use_forwarded_host = false ) {
+    return url_origin( $s, $use_forwarded_host ) . $s['REQUEST_URI'];
+}
+
 ?>
--- includes/templates/buddyforms/members/members-post-create.php
+++ includes/templates/buddyforms/members/members-post-create.php
@@ -22,19 +22,44 @@ if(bp_current_action() == 'revision'){
 $form_slug = $bp->current_component;
 //echo do_shortcode('[buddyforms_form post_type="'.$post_type.'" form_slug="'.$form_slug.'" post_id="'.$post_id.'" ]');
 
+// for create sub nav for child tabs.
+if ( $bp->current_action != 'create' && ! empty( strpos($bp->current_action, '-create') )  ) {
+        $patterns[0] = '/-create/';
+        $replacements[0] = '';
+        $form_slug = preg_replace($patterns, $replacements, $bp->current_action);
+}
+
+// set formslug and post type
 foreach ($buddyforms as $key => $member_form) {
     
     if ( $member_form['slug']  . '-create' ==  $bp->current_action ) {
-        $post_type = $member_form['post_type'];
+        $post_type = $member_form['post_type']; // get post type for child tab 
     }
-    // if ( $member_form['slug'] . '-edit' ==  $bp->current_action ) {
-    //     $post_type = $member_form['post_type'];
-    // }
-    if ( 'noparent' != $member_form['profiles_parent_tab'] && $member_form['slug']  . '-create' ==  $bp->current_action ) {
-        $form_slug = $member_form['slug'];
+    if ( 'edit' ==  $bp->current_action ) {
+        $post_type = $member_form['post_type']; // get post type for child tab 
+        // $form_slug = $bp->current_component;
     }
-    if ( 'noparent' != $member_form['profiles_parent_tab'] && $member_form['slug'] . '-edit' ==  $bp->current_action ) {
-        $form_slug = $member_form['slug'];
+    if ( 'noparent' != $member_form['profiles_parent_tab'] && 'edit' ==  $bp->current_action ) {
+        $form_slug =  $bp->current_component;
+    }
+}
+
+// for edit section child sub nav.
+foreach ($buddyforms as $key => $member_form) {
+    if ('edit' ==  $bp->current_action ) {
+        $absolute_url = full_url( $_SERVER );
+        $pieces = explode("/edit/", $absolute_url);
+        if (!empty($pieces[1])) {
+            $form_slug_string = $pieces[1];
+            $form_slug_array = explode("/", $form_slug_string);
+            $form_slug =  $form_slug_array[0];
+            $member_form['slug'];
+            if ($member_form['slug'] == $form_slug ) {
+                $post_type = $member_form['post_type'];
+                continue;
+            } 
+        }
+
     }
 }
 
