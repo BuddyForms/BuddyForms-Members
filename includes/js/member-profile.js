538fdf346c69284e400778476d516119b0d6cb68
Victor Marin <marin250189@gmail.com>
2019-03-26 22:45:59 -0400

    Fix: Broken Member Pages with latest BP and nouveau theme #24

--- includes/js/member-profile.js
+++ includes/js/member-profile.js
@@ -3,4 +3,16 @@ jQuery(document).ready(function (jQuery) {
     jQuery('#sub_nav_revison-personal-li').remove();
     jQuery('#sub_nav_page-personal-li').remove();
 
+    var wrapper =  document.createElement('div');
+    wrapper.setAttribute("id", "buddypress");
+    wrapper.setAttribute("class", "buddypress-wrap bp-dir-hori-nav");
+    var element = jQuery('.entry-content')[0];
+    if(element){
+        var parent = element.parentNode;
+        parent.replaceChild(wrapper, element);
+        wrapper.appendChild(element);
+    }
+
+
+
 });
