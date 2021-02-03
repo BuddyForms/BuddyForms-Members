6924ef2905f8b8cc39a085c35c12186cbd1da5fd
jnfDev <jairo.jnf.2010@gmail.com>
2021-02-03 17:20:19 -0400

    Add support to form_action use in PR 714 to fix an issue on the parent extension.

--- includes/templates/buddyforms/members/members-post-create.php
+++ includes/templates/buddyforms/members/members-post-create.php
@@ -7,22 +7,26 @@
 	$revision_id       = '';
 	$current_component = $bp->current_component;
 
-	$form_slug = $buddyforms_member_tabs[ $bp->current_component ][ $bp->current_action ];
+	$form_slug   = $buddyforms_member_tabs[ $bp->current_component ][ $bp->current_action ];
+	$form_action = false;
 
 	if ( bp_current_action() == $form_slug . '-create' ) {
 		if ( isset( $bp->action_variables[0] ) ) {
 			$post_parent_id = $bp->action_variables[0];
 		}
+		$form_action    = 'create';
 	}
 	if ( bp_current_action() == $form_slug . '-edit' ) {
 		if ( isset( $bp->action_variables[0] ) ) {
-			$post_id = $bp->action_variables[0];
+			$post_id     = $bp->action_variables[0];
 		}
+		$form_action = 'edit';
 	}
 	if ( bp_current_action() == $form_slug . '-revision' ) {
 		if ( isset( $bp->action_variables[1] ) ) {
 			$revision_id = $bp->action_variables[1];
 		}
+		$form_action = 'revision';
 	}
 
 	$args = array(
@@ -30,7 +34,8 @@
 		'post_id'     => $post_id,
 		'post_parent' => $post_parent_id,
 		'post_type'   => $post_type,
-		'revision_id' => $revision_id
+		'revision_id' => $revision_id,
+		'form_action' => $form_action,
 	);
 
 	buddyforms_create_edit_form( $args );
